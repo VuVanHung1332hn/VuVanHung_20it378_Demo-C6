057e51464d9a6014454a3b8ab7ed2bbf39779328
VuVanHung1332hn <crushlak7.0@gmail.com>
2023-03-14 20:30:57 +0700

    first commit

---
+++ Demo C6 WF/Demo C6 WF/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Demo_C6_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbThongTin = new System.Windows.Forms.TextBox();
+            this.txbHoTen = new System.Windows.Forms.TextBox();
+            this.txbDiaChi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbCCCD = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpkNgaysinh = new System.Windows.Forms.DateTimePicker();
+            this.cboGioiTinh = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txbThongTin
+            // 
+            this.txbThongTin.Location = new System.Drawing.Point(12, 254);
+            this.txbThongTin.Multiline = true;
+            this.txbThongTin.Name = "txbThongTin";
+            this.txbThongTin.Size = new System.Drawing.Size(497, 165);
+            this.txbThongTin.TabIndex = 9;
+            // 
+            // txbHoTen
+            // 
+            this.txbHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txbHoTen.Location = new System.Drawing.Point(138, 35);
+            this.txbHoTen.Name = "txbHoTen";
+            this.txbHoTen.Size = new System.Drawing.Size(336, 26);
+            this.txbHoTen.TabIndex = 0;
+            this.txbHoTen.TextChanged += new System.EventHandler(this.txbHoTen_TextChanged);
+            // 
+            // txbDiaChi
+            // 
+            this.txbDiaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txbDiaChi.Location = new System.Drawing.Point(138, 120);
+            this.txbDiaChi.Name = "txbDiaChi";
+            this.txbDiaChi.Size = new System.Drawing.Size(336, 26);
+            this.txbDiaChi.TabIndex = 0;
+            this.txbDiaChi.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label1.Location = new System.Drawing.Point(44, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Học Tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(44, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Địa chỉ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.Location = new System.Drawing.Point(262, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button2.Location = new System.Drawing.Point(380, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(44, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ngày sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(47, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "CCCD";
+            // 
+            // txbCCCD
+            // 
+            this.txbCCCD.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.txbCCCD.Location = new System.Drawing.Point(138, 163);
+            this.txbCCCD.Name = "txbCCCD";
+            this.txbCCCD.Size = new System.Drawing.Size(135, 24);
+            this.txbCCCD.TabIndex = 5;
+            this.txbCCCD.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(294, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Giới tính";
+            // 
+            // dtpkNgaysinh
+            // 
+            this.dtpkNgaysinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.dtpkNgaysinh.Location = new System.Drawing.Point(138, 79);
+            this.dtpkNgaysinh.Name = "dtpkNgaysinh";
+            this.dtpkNgaysinh.Size = new System.Drawing.Size(336, 24);
+            this.dtpkNgaysinh.TabIndex = 7;
+            // 
+            // cboGioiTinh
+            // 
+            this.cboGioiTinh.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.cboGioiTinh.FormattingEnabled = true;
+            this.cboGioiTinh.Location = new System.Drawing.Point(363, 162);
+            this.cboGioiTinh.Name = "cboGioiTinh";
+            this.cboGioiTinh.Size = new System.Drawing.Size(111, 26);
+            this.cboGioiTinh.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 420);
+            this.Controls.Add(this.txbThongTin);
+            this.Controls.Add(this.cboGioiTinh);
+            this.Controls.Add(this.dtpkNgaysinh);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbCCCD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbDiaChi);
+            this.Controls.Add(this.txbHoTen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txbThongTin;
+        private System.Windows.Forms.TextBox txbHoTen;
+        private System.Windows.Forms.TextBox txbDiaChi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbCCCD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpkNgaysinh;
+        private System.Windows.Forms.ComboBox cboGioiTinh;
+    }
+}
+
---
+++ Demo C6 WF/Demo C6 WF/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_C6_WF
+{
+    public partial class Form1 : Form
+    {
+        List<string> listGioiTinh = new List<string>() { "Nam", "Nu", "Khong xac dinh" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            if (KiemTraNhap())
+            {
+                txbThongTin.Text = "Ho ten: " + txbHoTen.Text + Environment.NewLine
+                                    + "Ngay sinh: " + dtpkNgaysinh.Value.ToShortDateString() + Environment.NewLine
+                                    + "Gioi tinh: " + cboGioiTinh.SelectedItem + Environment.NewLine
+                                    + "Dia chi: " + txbDiaChi.Text + Environment.NewLine
+                                    + "CCCD: " + txbCCCD.Text;
+
+            }
+         
+        }
+        bool KiemTraNhap()
+        {
+            string cccd = txbCCCD.Text;
+            long ketqua;
+
+            char[] mangCCCD = cccd.ToCharArray();
+
+            if (txbHoTen.Text == "")
+            {
+                MessageBox.Show("Hay nhap ho ten", "Thong bao");
+                txbHoTen.Focus();
+                return false;
+            }
+            if (txbDiaChi.Text == "")
+            {
+                MessageBox.Show("Hay nhap dia chi", "Thong bao");
+                txbDiaChi.Focus();
+                return false;
+            }
+            if (!(long.TryParse(cccd, out ketqua)))
+            {
+                MessageBox.Show("Hay nhap dung so CC", "Thong bao");
+                txbCCCD.Focus();
+                return false;
+            }
+            if (ketqua < 0)
+            {
+                MessageBox.Show("So CCCD khong duoc am", "Thong bao");
+                txbCCCD.Focus();
+                return false;
+            }
+            if (mangCCCD.Length != 12)
+            {
+                MessageBox.Show("So CCCD phai du 12 so", "Thong bao");
+                txbCCCD.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboGioiTinh.DataSource = listGioiTinh;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Ban co muon thoat chuong trinh?", "Canh bao", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+             e.Cancel = true;
+
+        }
+
+        private void txbHoTen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
